789b7ce6bbb712bdd3c0629244af0dd9f148e62b
quan.nguyen <nguyendinhanhquan0305@gmail.com>
2025-07-09 19:08:51 +0700

    feat: test cicd

--- src/Components/TermPolicy/TermPolicy.tsx
+++ src/Components/TermPolicy/TermPolicy.tsx
@@ -16,7 +16,7 @@ const TermPolicy = () => {
 			}}
 		>
 			<Flex gap={12}>
-				<span>Terms of Use 111</span>
+				<span>Terms of Use 1112</span>
 				<span>Privacy Policy</span>
 			</Flex>
 			<span>Copyrightⓒ(Inc)UniVini. All rights reserved.</span>
