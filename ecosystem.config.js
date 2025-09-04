8fa790810886084cb8be2170f7b9305e2d050e62
quan.nguyen <quan.nguyen@mail.finviet.com.vn>
2025-09-04 20:41:01 +0700

    Feat: update start script to use production environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         {
             name: "web",              // tên app hiển thị trong pm2 list
             script: "npm",            // chạy npm
-            args: "run start",        // tương đương: npm run start
+            args: "run start:prod",        // tương đương: npm run start
             env: {
                 NODE_ENV: "production", // môi trường production
                 PORT: 3030              // có thể set port ở đây (Next.js đọc biến PORT)
