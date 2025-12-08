d77356af6707d227a8f73e0a482d55ec00dfc70c
Tran Ninh Hieu <hieukute123a@gmail.com>
2025-12-08 21:54:04 +0700

    build: push 1 commit

--- scripts/generate-build-info.js
+++ scripts/generate-build-info.js
@@ -3,7 +3,7 @@ const path = require('path')
 const buildInfo = {
 	version: Date.now(), // timestamp build
 }
-
+// push 1 commit
 const outPath = path.join(__dirname, '../src/build-info.json')
 fs.writeFileSync(outPath, JSON.stringify(buildInfo, null, 2))
 
