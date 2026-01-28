0751862b0c2f4db32ff119e5b41745982818c620
Tran Ninh Hieu <hieukute123a@gmail.com>
2026-01-28 20:08:13 +0700

    fix: bug UNIWEB-442

--- src/Components/Custom/CImage/CImage.tsx
+++ src/Components/Custom/CImage/CImage.tsx
@@ -1,15 +1,37 @@
 import { Flex, Image, ImageProps } from 'antd'
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
 
 import classes from './CImage.module.scss'
+
+const MAX_RETRY = 2
+const RETRY_DELAY = 1000
+
 const CImage = (_props: ImageProps) => {
 	const { src, ...props } = _props
+	const [retry, setRetry] = useState(0)
+	const [imgSrc, setImgSrc] = useState(src)
+
+	useEffect(() => {
+		setRetry(0)
+		setImgSrc(src)
+	}, [src])
+
+	const handleError = () => {
+		if (retry < MAX_RETRY && src) {
+			setTimeout(() => {
+				setRetry((r) => r + 1)
+				setImgSrc(`${src}?retry=${Date.now()}`)
+			}, RETRY_DELAY)
+		}
+	}
+
 	return (
 		<Flex align="center" justify="center" className={classes.wrapper}>
 			<Image
 				className={classes.image}
-				src={src || '/images/defaultCover.png'}
+				src={imgSrc || '/images/defaultCover.png'}
 				preview={false}
+				onError={handleError}
 				{...props}
 			/>
 		</Flex>
