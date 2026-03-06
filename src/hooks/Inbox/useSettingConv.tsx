315d9e8b3a35abc5af78137725c0a6d543fe3ed2
Tran Ninh Hieu <hieukute123a@gmail.com>
2026-03-06 21:52:48 +0700

    fix: bug chatbox

--- src/hooks/Inbox/useSettingConv.tsx
+++ src/hooks/Inbox/useSettingConv.tsx
@@ -91,7 +91,9 @@ export default function useSettingConv({
 				setMedias((prev: any[]) => {
 					const contents = isNew ? [] : prev
 					const resData = (_rows || []).flatMap((item) =>
-						(item?.medias || []).filter((i) => i.type === 'IMAGE'),
+						(item?.medias || []).filter((i) =>
+							['IMAGE', 'VIDEO'].includes(i.type),
+						),
 					)
 					const newData = uniqueArray([...contents, ...resData], 'url') || []
 					return newData
