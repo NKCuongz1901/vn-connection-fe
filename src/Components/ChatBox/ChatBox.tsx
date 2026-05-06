e8825454e18d76a766ce8f4f9ddfa8c5303c6fcd
NKCuongz1901 <cuong19012002@gmail.com>
2026-05-06 11:06:38 +0700

    feat: appy text to speech, translate to inbox and hangout

--- src/Components/ChatBox/ChatBox.tsx
+++ src/Components/ChatBox/ChatBox.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { IconCircleXFilled } from '@tabler/icons-react'
-import { Dropdown, Flex, Skeleton } from 'antd'
+import { Dropdown, Flex, Menu, Skeleton } from 'antd'
 import clsx from 'clsx'
 import dayjs from 'dayjs'
 import { memo, useCallback, useLayoutEffect, useRef, useState } from 'react'
@@ -25,10 +25,18 @@ import CLoading from '../Custom/CLoading/CLoading'
 import CTextSpecial from '../Custom/CTextSpecial'
 import CUploadMuti from '../Custom/CUploadMuti'
 
-import { specialTypeMessage } from '@/Variable/common.variable'
+import { languageOpts, specialTypeMessage } from '@/Variable/common.variable'
 
 import MoreIcon from '@/svg/MoreIcon'
 import classes from './ChatBox.module.scss'
+import MicroPhoneIcon from '@/svg/MicroPhoneIcon'
+import AudioRecorder from '../AudioRecorder'
+import DotIcon from '@/svg/DotIcon'
+import CInput from '../Custom/CInput'
+import VisualizerWithPlay from '../VisualizerWithPlay'
+import CcIcon from '@/svg/CcIcon'
+import TranslateIcon from '@/svg/TranslateIcon'
+import VolumeIcon from '@/svg/VolumeIcon'
 
 interface ChatBoxProps {
 	isDisabledChat?: boolean
@@ -63,23 +71,41 @@ const ChatBox = ({
 	convId,
 }: ChatBoxProps) => {
 	const {
+		listTranslateLoading,
+		isAudio,
 		_refInput,
 		activeSticker,
 		showSticker,
 		stickerList,
-		reactList,
-		openReact,
 		text,
 		reply,
+		listSpToText,
+		listSpToTextLoading,
+		listTextToSpeechLoading,
+		playAudioId,
+		listTranslate,
+		openReact,
+		reactList,
+		language,
+		searchCountry,
+
+		setSearchCountry,
+		onStopAudio,
+
 		setReply,
-		setText,
 		setOpenReact,
+		setIsAudio,
+		setText,
 		setActiveSticker,
 		setShowSticker,
 		onScroll,
 		onGetMenus,
-		onOpenReact,
+		onAddSpToText,
+		onAddTextToSpeech,
+		onAddTranslate,
 		onAddReact: onActionReact,
+		onOpenReact,
+		onChangeLanguage,
 	} = useChatBox({ onLoadMore, type, onActionMessage, onAddReact })
 
 	const [fileList, setFileList] = useState([])
@@ -217,60 +243,145 @@ const ChatBox = ({
 		)
 	}
 
-	const _renderContentChat = ({
-		type,
-		content,
-		medias,
-		isLast,
-		created_at,
-		parent,
-		user,
-		mentions,
-		reactions,
-	}) => {
+	const _renderContentChat = (item) => {
+		const {
+			id,
+			type,
+			content,
+			mentions,
+			medias,
+			parent,
+			user,
+			user_id,
+			isTemp,
+			reactions,
+			created_at,
+		} = item || {}
+
 		const { name } = user || {}
+		const spToText = listSpToText[id]
+		const trans = listTranslate[id]
+		const isMe = getUserInfo('id') === user_id
+		const isMemberAction = specialTypeMessage.includes(type)
+		const typeMedia = medias?.[0]?.type
+		const loadingSpToText = !!listSpToTextLoading[id]
+
 		switch (type) {
 			case 'TEXT':
 				return (
 					<div className={classes.text}>
 						{_renderParentItem(parent)}
 						<CTextSpecial data={content} mentions={mentions} />
-						{isLast && (
-							<div className={classes.time}>
-								{created_at ? dayjs(created_at).format('HH:mm') : ''}
+						{trans && (
+							<div className={classes.translateWrapper}>
+								<Flex vertical className={classes.translateContainer}>
+									<div className={classes.translateText}>{trans}</div>
+									<Flex className={classes.translateOpt}>
+										<span className={classes.translateOptTitle}>
+											UniVini AI
+										</span>
+										<div className={classes.dot}>
+											<DotIcon />
+										</div>
+										<Dropdown
+											trigger={['click']}
+											onOpenChange={() => {
+												setSearchCountry('')
+											}}
+											dropdownRender={() => (
+												<Flex vertical className={classes.wrapperMenuCoutry}>
+													<CInput
+														placeholder="Search language"
+														size="small"
+														value={searchCountry}
+														onChange={(e) => setSearchCountry(e.target.value)}
+														style={{ height: 32 }}
+													/>
+													<div className={classes.dropdownChangeLanguage}>
+														<Menu
+															selectedKeys={[language]}
+															items={languageOpts
+																.filter((i) =>
+																	i.searchLabel
+																		.toLocaleLowerCase()
+																		.includes(
+																			searchCountry.toLocaleLowerCase(),
+																		),
+																)
+																.map((i) => ({
+																	key: i.lang,
+																	label: i.label,
+																	onClick: () =>
+																		onChangeLanguage({ item, code: i.lang }),
+																}))}
+														/>
+													</div>
+												</Flex>
+											)}
+											disabled={isTemp || isMemberAction}
+										>
+											<b
+												style={{
+													color: '#006B35',
+													fontSize: 12,
+													cursor: 'pointer',
+												}}
+											>
+												Change language
+											</b>
+										</Dropdown>
+									</Flex>
+								</Flex>
 							</div>
 						)}
+						<div className={classes.time}>
+							{created_at ? dayjs(created_at).format('HH:mm') : ''}
+						</div>
 						{_renderReactView(reactions)}
 					</div>
 				)
+
 			case 'STICKER':
 				return (
 					<Flex vertical className={classes.sticker}>
 						<CImage src={content} />
-						{isLast && (
-							<div className={classes.time}>
-								{created_at ? dayjs(created_at).format('HH:mm') : ''}
-							</div>
-						)}
+						<div className={classes.time}>
+							{created_at ? dayjs(created_at).format('HH:mm') : ''}
+						</div>
 						{_renderReactView(reactions)}
 					</Flex>
 				)
+
 			case 'MEDIAS': {
 				const isMulti = isArray(medias, 2)
-				const Content = (medias || []).map((media, index) => {
-					const isImg = media?.type === 'IMAGE'
-					return (
-						<Flex className={classes.media} key={index}>
-							{isImg ? (
-								<CImage preview src={media.url} />
-							) : (
-								<video controls>
-									<source src={media.url} type="video/mp4" />
-								</video>
-							)}
-						</Flex>
-					)
-				})
+				let Content = null
+
+				switch (medias?.[0]?.type) {
+					case 'AUDIO':
+						Content = (medias || []).map((media, index) => (
+							<Flex key={index}>
+								<VisualizerWithPlay item={item} src={media.url} />
+							</Flex>
+						))
+						break
+
+					default:
+						Content = (medias || []).map((media, index) => {
+							const { type } = media || {}
+							const isImg = type === 'IMAGE'
+							return (
+								<Flex className={classes.media} key={index}>
+									{isImg ? (
+										<CImage preview src={media.url} />
+									) : (
+										<video controls>
+											<source src={media.url} type="video/mp4" />
+										</video>
+									)}
+								</Flex>
+							)
+						})
+				}
 
 				return (
 					<Flex className={classes.mediasWrapper} vertical>
@@ -280,21 +391,22 @@ const ChatBox = ({
 						>
 							{Content}
 						</Flex>
-						{isLast && (
-							<div className={classes.time}>
-								{created_at ? dayjs(created_at).format('HH:mm') : ''}
-							</div>
-						)}
+						{!!spToText && <Flex className={classes.spToText}>{spToText}</Flex>}
+						<div className={classes.time}>
+							{created_at ? dayjs(created_at).format('HH:mm') : ''}
+						</div>
 						{_renderReactView(reactions)}
 					</Flex>
 				)
 			}
+
 			case 'MEMBER_ACCEPT':
 			case 'TITLE_CHANGE':
 			case 'ADDRESS':
 			case 'MEMBER_LEAVE':
 			case 'MEMBER_JOIN':
 				return <Flex className={classes.memberAccept}>{content}</Flex>
+
 			case 'PIN':
 			case 'UNPIN':
 				return (
@@ -304,6 +416,7 @@ const ChatBox = ({
 						</div>
 					</Flex>
 				)
+
 			default:
 				return <Flex className={classes.memberAccept}>{type}</Flex>
 		}
@@ -379,6 +492,8 @@ const ChatBox = ({
 		const isMe = getUserInfo('id') === user_id
 		const isMemberAction = specialTypeMessage.includes(type)
 		const isNot = isMe || isMemberAction
+		const typeMedia = item?.medias?.[0]?.type
+		const loadingSpToText = !!listSpToTextLoading[id]
 
 		return (
 			<Flex
@@ -422,6 +537,54 @@ const ChatBox = ({
 							>
 								{!(isTemp || isMemberAction) && (
 									<Flex className={classes.moreIconWrapper}>
+										{!isMe && typeMedia === 'AUDIO' && (
+											<Flex
+												className={clsx(classes.moreIcon, {
+													[classes.disabled]: loadingSpToText,
+												})}
+												onClick={() => !loadingSpToText && onAddSpToText(item)}
+											>
+												{loadingSpToText ? <CLoading /> : <CcIcon />}
+											</Flex>
+										)}
+
+										{type === 'TEXT' && !isMe && (
+											<Flex
+												className={clsx(classes.moreIcon, {
+													[classes.disabled]: listTranslateLoading[id],
+												})}
+												onClick={() => onAddTranslate(item)}
+											>
+												{listTranslateLoading[id] ? (
+													<CLoading />
+												) : (
+													<TranslateIcon />
+												)}
+											</Flex>
+										)}
+
+										{type === 'TEXT' && !isMe && (
+											<Flex
+												className={clsx(classes.moreIcon, {
+													[classes.disabled]: listTextToSpeechLoading[id],
+													[classes.isPlaying]: playAudioId === id,
+												})}
+												onClick={() => {
+													if (playAudioId === id) {
+														onStopAudio(id)
+													} else if (!listTextToSpeechLoading[id]) {
+														onAddTextToSpeech(item)
+													}
+												}}
+											>
+												{listTextToSpeechLoading[id] ? (
+													<CLoading />
+												) : (
+													<VolumeIcon />
+												)}
+											</Flex>
+										)}
+
 										<Dropdown
 											trigger={['click']}
 											menu={{ items: onGetMenus({ item, isMe }) }}
@@ -431,6 +594,7 @@ const ChatBox = ({
 												<MoreIcon />
 											</Flex>
 										</Dropdown>
+
 										<Flex
 											className={clsx(classes.moreIcon)}
 											onClick={() => onOpenReact(item)}
@@ -575,82 +739,115 @@ const ChatBox = ({
 						</Flex>
 					))}
 			</Flex>
-
-			{!isDisabledChat ? (
-				<>
-					{reply && _renderReply()}
-
-					<Flex className={clsx(classes.chatBox)}>
-						<Flex className={classes.chooseImgContent}>
-							{fileList.map((i) => {
-								const { url, type } = i || {}
-								const isImg = type === 'IMAGE'
-								return (
-									<Flex key={url} className={classes.chooseImgItem}>
-										{isImg ? (
-											<CImage preview={true} src={url} />
-										) : (
-											<video controls>
-												<source src={url} type="video/mp4" />
-											</video>
-										)}
-										<Flex
-											className={classes.chooseImgCancel}
-											onClick={() =>
-												setFileList((prev) =>
-													prev.filter((prev) => prev.url !== url),
-												)
-											}
-										>
-											<IconCircleXFilled />
-										</Flex>
+			{reply && _renderReply()}
+			<Flex className={clsx(classes.chatBox)}>
+				<Flex className={classes.chooseImg}>
+					<Flex className={classes.chooseImgContent}>
+						{fileList.map((i) => {
+							const { url, type } = i || {}
+							const isImg = type === 'IMAGE'
+							return (
+								<Flex key={url} className={classes.chooseImgItem}>
+									{isImg ? (
+										<CImage preview={true} src={url} />
+									) : (
+										<video controls>
+											<source src={url} type="video/mp4" />
+										</video>
+									)}
+									<Flex
+										className={classes.chooseImgCancel}
+										onClick={() =>
+											setFileList((prev) =>
+												prev.filter((prev) => prev.url !== url),
+											)
+										}
+									>
+										<IconCircleXFilled />
 									</Flex>
-								)
-							})}
-						</Flex>
-
-						<Flex className={classes.chooseImg}>
-							<CUploadMuti
-								accept="image/*,video/*"
-								fileList={fileList.map((i) => i.file)}
-								onChange={({ file: _file, fileList: newList }) => {
-									hangleImportImg(newList)
-								}}
-							>
-								<ImageIcon />
-							</CUploadMuti>
-						</Flex>
-
-						<CInputTag
-							ref={_refInput}
-							id={convId}
-							value={text}
-							suffix={_renderIconHappy()}
-							placeholder="Enter your text ..."
-							onChange={(e) => setText(e.target.value)}
-							disabled={fileList?.length > 0}
-							onSendMessage={(e) => {
-								if (!isMobile() && e.key === 'Enter' && !e.shiftKey) {
-									e.preventDefault()
-									handleSubmitMessage()
-								}
-							}}
-						/>
-
-						<Flex className={classes.sendButton} onClick={handleSubmitMessage}>
-							<SendIcon />
-						</Flex>
+								</Flex>
+							)
+						})}
 					</Flex>
+					<CUploadMuti
+						fileList={fileList.map((i) => i.file)}
+						onChange={({ file: _file, fileList: newList }) => {
+							hangleImportImg(newList)
+						}}
+						accept="image/*,video/*"
+					>
+						<ImageIcon />
+					</CUploadMuti>
+				</Flex>
+				<CInputTag
+					ref={_refInput}
+					id={convId}
+					allowClear={false}
+					value={text}
+					style={{ height: 40 }}
+					suffix={_renderIconHappy()}
+					placeholder="Enter your text ..."
+					disabled={fileList?.length > 0}
+					onChange={(e) => setText(e.target.value)}
+					onSendMessage={(e) => {
+						if (e.key === 'Enter' && !e.shiftKey) {
+							e.preventDefault()
+							if (!!text.trim()) {
+								setText('')
+								setReply(null)
+								onSendMessage({
+									type: 'TEXT',
+									content: text,
+									parent: reply,
+								})
+							}
+						}
+					}}
+				/>
 
-					{_renderSticketList()}
-				</>
-			) : (
-				!loadingPage && (
-					<Flex className={clsx(classes.chatBox, classes.deletedChat)}>
-						Unavailable to send message for this person
+				{text || isArray(fileList, 1) ? (
+					<Flex
+						className={classes.sendButton}
+						onClick={() => {
+							if (!!text.trim() || isArray(fileList, 1)) {
+								setText('')
+								setReply(null)
+								onSendMessage({
+									type: 'TEXT',
+									content: text,
+									parent: reply,
+									medias: fileList,
+								})
+								setFileList([])
+							}
+						}}
+					>
+						<SendIcon />
 					</Flex>
-				)
+				) : (
+					<Flex
+						className={clsx(classes.micro, { [classes.isAudioMicro]: isAudio })}
+						onClick={() => setIsAudio((prev) => !prev)}
+					>
+						<MicroPhoneIcon />
+					</Flex>
+				)}
+			</Flex>
+			{!(text || isArray(fileList, 1)) && isAudio && (
+				<div>
+					<AudioRecorder
+						onClose={() => setIsAudio(false)}
+						onComplete={(e) => {
+							onSendMessage({
+								type: 'TEXT',
+								parent: reply,
+								audio: e,
+							})
+						}}
+					/>
+				</div>
 			)}
+			{_renderSticketList()}
 		</div>
 	)
 }
--- src/hooks/ChatBox/useChatBox.tsx
+++ src/hooks/ChatBox/useChatBox.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useRef, useState } from 'react'
 
 import { getListSticket } from '@/apis/postApis'
-import { getReact } from '@/apis/conversationApis'
+import { getReact, textToSpeech, translate } from '@/apis/conversationApis'
 
 import { useModal } from '@/context/ModalContext'
 import { ItemType } from 'antd/es/menu/interface'
 import { copyToClipboard } from '@/ultis/string'
 import { ReactionPtops } from '@/interface/Conversation/Conversation.interface'
+import { handleUploadFile } from '@/apis/uploadApis'
+import { playAudio, stopAudio } from '@/ultis/file'
+import { updateUserProfile } from '@/apis/userApis'
 
 type useHangoutChatProps = {
 	type?: string
@@ -21,6 +24,8 @@ export default function useChatBox({
 }: useHangoutChatProps) {
 	const { openError, openSuccess } = useModal()
 	const _refInput = useRef() as any
+	const prevAudioId = useRef('')
+
 	const [stickerList, setStickerList] = useState([]) as any[]
 	const [reactList, setReactList] = useState<ReactionPtops[]>([])
 	const [openReact, setOpenReact] = useState() as any
@@ -28,6 +33,117 @@ export default function useChatBox({
 	const [showSticker, setShowSticker] = useState(false)
 	const [text, setText] = useState('')
 	const [reply, setReply] = useState() as any
+
+	const [searchCountry, setSearchCountry] = useState('')
+	const [isAudio, setIsAudio] = useState(false)
+	const [listSpToText, setListSpToText] = useState({})
+	const [listSpToTextLoading, setListSpToTextLoading] = useState({})
+
+	const [listTextToSpeech, setListTextToSpeech] = useState({})
+	const [listTextToSpeechLoading, setListTextToSpeechLoading] = useState({})
+
+	const [listTranslate, setListTranslate] = useState({})
+	const [listTranslateLoading, setListTranslateLoading] = useState({})
+
+	const [playAudioId, setPlayAudioId] = useState('')
+	const [language, setLanguage] = useState('en')
+
+	const handleAddMp3 = async (url) => {
+		const res = await fetch('/api/proxy', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				url: url,
+				fileName: 'audio.mp3',
+			}),
+		})
+
+		const blob = await res.blob()
+		const file = new File([blob], 'audio.mp3', { type: blob.type })
+		return file
+	}
+
+	const handleAddSpToText = async (item) => {
+		const { id, medias } = item || {}
+		setListSpToTextLoading((prev) => ({ ...prev, [id]: true }))
+		try {
+			const res1 = await handleAddMp3(medias[0]?.url)
+
+			// giả lập như file được chọn từ input
+			const res: any = await handleUploadFile(res1)
+			const text = res?.results?.object?.text
+			setListSpToText((prev) => ({ ...prev, [id]: text }))
+		} catch (error) {
+			openError(error)
+		} finally {
+			setListSpToTextLoading((prev) => ({ ...prev, [id]: false }))
+		}
+	}
+
+	const handleAddTextToSpeech = async (item) => {
+		const { id, content } = item || {}
+		if (listTextToSpeech[id]) {
+			setPlayAudioId(id)
+
+			return
+		}
+		setListTextToSpeechLoading((prev) => ({ ...prev, [id]: true }))
+		try {
+			const payload = {
+				text: content,
+			}
+			const res: any = await textToSpeech({ payload })
+			const url = res?.results?.object?.url
+			setListTextToSpeech((prev) => ({ ...prev, [id]: url }))
+			setPlayAudioId(id)
+		} catch (error) {
+			openError(error)
+		} finally {
+			setListTextToSpeechLoading((prev) => ({ ...prev, [id]: false }))
+		}
+	}
+
+	const handleAddTranslate = async (item, code?: string) => {
+		const { id, content } = item || {}
+
+		setListTranslateLoading((prev) => ({ ...prev, [id]: true }))
+		try {
+			const payload = {
+				text: content,
+				targetLanguage: code || language || 'en',
+			}
+			const res: any = await translate({ payload })
+			const example = res?.results?.object?.translated_text
+			setListTranslate((prev) => ({ ...prev, [id]: example }))
+		} catch (error) {
+			openError(error)
+		} finally {
+			setListTranslateLoading((prev) => ({ ...prev, [id]: false }))
+		}
+	}
+
+	const handlePlayAudio = (playAudioId) => {
+		stopAudio(prevAudioId.current)
+
+		playAudio(playAudioId, listTextToSpeech[playAudioId], () =>
+			setPlayAudioId(''),
+		)
+	}
+	const handleStopAudio = (id) => {
+		stopAudio(id)
+		setPlayAudioId('')
+	}
+
+	const handleChangeLanguage = async ({ item, code }) => {
+		try {
+			await updateUserProfile({ language_for_translate: code })
+			setLanguage(code || 'en')
+			handleAddTranslate(item, code)
+		} catch (error) {
+			openError(error)
+		}
+	}
+
 	const handleScroll = (e: any) => {
 		const clientHeight = e.target.clientHeight
 		const scrollHeight = e.target.scrollHeight
@@ -148,7 +264,18 @@ export default function useChatBox({
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
+	useEffect(() => {
+		if (playAudioId) {
+			handlePlayAudio(playAudioId)
+			prevAudioId.current = playAudioId
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [playAudioId])
+
 	return {
+		isAudio,
+		listTranslateLoading,
+
 		_refInput,
 		activeSticker,
 		showSticker,
@@ -157,6 +284,17 @@ export default function useChatBox({
 		openReact,
 		text,
 		reply,
+		listSpToText,
+		listSpToTextLoading,
+		listTextToSpeechLoading,
+		listTextToSpeech,
+		playAudioId,
+		listTranslate,
+		language,
+		searchCountry,
+
+		setSearchCountry,
+		setIsAudio,
 		setReply,
 		setText,
 		setOpenReact,
@@ -165,6 +303,12 @@ export default function useChatBox({
 		onScroll: handleScroll,
 		onGetMenus: handleGetMenus,
 		onOpenReact: handleOpenReact,
+		onAddSpToText: handleAddSpToText,
+		onAddTextToSpeech: handleAddTextToSpeech,
+		onPlayAudio: handlePlayAudio,
+		onStopAudio: handleStopAudio,
+		onAddTranslate: handleAddTranslate,
 		onAddReact: handleAddReact,
+		onChangeLanguage: handleChangeLanguage,
 	}
 }
