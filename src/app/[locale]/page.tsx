3a82addb16c1664832f8f8424295fd65606c1982
quan.nguyen <quan.nguyen1@finviet.com.vn>
2026-05-08 16:23:44 +0700

    chore: deploy

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,5 @@ export default function Term() {
 	const headersList = headers()
 
 	const pathname = headersList.get('x-x-pathname') || ''
-	// console.log('🎇🧧🧧🧧🎇 TrieuNinhHan ~ Term ~ pathname::', pathname)
 	return <div className="p-10">Welcome to https://univini.com/</div>
 }
