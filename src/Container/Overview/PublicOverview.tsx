0d6a75931d8b8238b05bb4bde678a12241fbb522
NKCuongz1901 <cuong19012002@gmail.com>
2026-07-21 21:40:35 +0700

    feat: build upcoming activity in public overview

--- src/Container/Overview/PublicOverview.tsx
+++ src/Container/Overview/PublicOverview.tsx
@@ -1,20 +1,30 @@
 'use client'
 
 import clsx from 'clsx'
-import { Flex } from 'antd'
+import { Flex, Skeleton } from 'antd'
 import { memo } from 'react'
 
 import OverviewLockedSection from '@/Components/Overview/OverviewLockedSection'
 import CAvatarBandage from '@/Components/Custom/CAvatarBandage'
 import CButtonCreate from '@/Components/Custom/CButtonCreate'
+import CDatePickerRanger from '@/Components/Custom/CDatePickerRanger'
+import CInput from '@/Components/Custom/CInput'
+import CSelect from '@/Components/Custom/CSelect'
 import EventTitle from '@/Components/Event/EventTitle'
+import ItemEvent from '@/Components/Event/ItemEvent'
+import usePublicOverview from '@/hooks/Overview/usePublicOverview'
 import useRequireLogin from '@/hooks/useRequireLogin'
 import EventIcon from '@/svg/Event'
+import MarkIcon from '@/svg/MarkIcon'
 import Message2Icon from '@/svg/Message2Icon'
 import MicroPhoneIcon from '@/svg/MicroPhoneIcon'
+import NotFound from '@/svg/NotFound'
 import Party from '@/svg/Party'
 import People from '@/svg/People'
+import SearchIcon from '@/svg/SearchIcon'
 import UpcomingEvent from '@/svg/UpcomingEvent'
+
+import { arrayFrom, isArray } from '@/ultis/array'
 
 import { mappingEventTitle } from '@/Variable/event.variable'
 import {
@@ -22,6 +32,7 @@ import {
 	OVERVIEW_GUEST_CHAT_ROOM_COUNT,
 } from '@/Variable/overviewGuestChatRooms.variable'
 import { LEFT_FLAG } from '@/Variable/countryVariable'
+import { radiusOpts, typeEvent } from '@/Variable/select.variable'
 import { mainRoutes } from '@/routes/MainRoutes'
 
 import classes from './Overview.module.scss'
@@ -31,6 +42,15 @@ const GUEST_ACTIVITY_COUNT = 0
 
 function PublicOverview() {
 	const { requireLogin } = useRequireLogin()
+	const {
+		listPost,
+		loading,
+		total,
+		filters,
+		onChangeFilter,
+		onChangeKeyword,
+		onScrollList,
+	} = usePublicOverview()
 
 	const _renderHangout = () => (
 		<Flex vertical className={classes.hangout}>
@@ -151,20 +171,103 @@ function PublicOverview() {
 		</Flex>
 	)
 
+	const _renderFilter = () => {
+		const { radius, date, categories, title } = filters
+		return (
+			<Flex vertical className={classes.renderFilter}>
+				<Flex className={classes.filter}>
+					<Flex className={classes.search}>
+						<CInput
+							value={title}
+							placeholder="Search by keywords"
+							style={{ background: '#fff', borderRadius: 40, height: 44 }}
+							prefix={<SearchIcon />}
+							onChange={onChangeKeyword}
+						/>
+					</Flex>
+					<Flex>
+						<CDatePickerRanger
+							isWhite
+							style={{ background: '#fff', borderRadius: 40 }}
+							disabled={loading}
+							value={date}
+							onChange={onChangeFilter('date')}
+						/>
+					</Flex>
+					<Flex className={classes.distance}>
+						<CSelect
+							isMaxRadius
+							isWhite
+							style={{ background: '#fff', borderRadius: 40 }}
+							disabled={loading}
+							value={radius}
+							options={radiusOpts}
+							placeholder="Choose distance"
+							prefix={<MarkIcon />}
+							onChange={onChangeFilter('radius')}
+						/>
+					</Flex>
+				</Flex>
+				<Flex className={classes.categoryWrapper}>
+					{typeEvent.map((item) => {
+						const { value, label } = item
+						return (
+							<Flex
+								key={value}
+								className={clsx(classes.categoryItem, {
+									[classes.categoryActive]: (categories || []).includes(value),
+									[classes.disabled]: loading,
+								})}
+								onClick={() => !loading && onChangeFilter('categories')(value)}
+							>
+								{label}
+							</Flex>
+						)
+					})}
+				</Flex>
+			</Flex>
+		)
+	}
+
 	const _renderUpcomingEvent = () => (
 		<Flex className={classes.wrapperUp} vertical>
 			<Flex className={classes.title} onClick={requireLogin}>
 				<EventTitle
 					hiddenAdd
 					label={mappingEventTitle[mainRoutes.upcomingEvent]}
+					number={total}
 					icon={<UpcomingEvent />}
-					hiddenNumber
 				/>
 			</Flex>
-			<OverviewLockedSection
-				description="Explore upcoming activities in your area"
-				onLogin={requireLogin}
-			/>
+			{_renderFilter()}
+			<Flex className={classes.wrapperItemUp} onScroll={onScrollList}>
+				{listPost.map((data) => (
+					<ItemEvent
+						key={data.id}
+						data={data}
+						type={mainRoutes.publicEvent}
+					/>
+				))}
+				{loading &&
+					arrayFrom(3).map((_, index) => (
+						<Skeleton.Input
+							key={index}
+							active
+							className={classes.contentBody}
+						/>
+					))}
+				{!loading && !isArray(listPost, 1) && (
+					<Flex className={classes.eventNotFound} vertical>
+						<NotFound />
+						<span className={classes.eventNotFoundTitle}>
+							No activities here yet
+						</span>
+						<span className={classes.eventNotFoundLabel}>
+							Try another location or create a meetup to bring people together.
+						</span>
+					</Flex>
+				)}
+			</Flex>
 		</Flex>
 	)
 
