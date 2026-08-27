4fba00550c2367bee4af74afbfc7de1c1d89a774
NKCuongz1901 <cuong19012002@gmail.com>
2026-08-27 23:17:58 +0700

    fix: fix display preview edit message

--- src/Components/ChatBox/ChatBox.tsx
+++ src/Components/ChatBox/ChatBox.tsx
@@ -978,12 +978,18 @@ const ChatBox = ({
 
 	const _renderEdit = () => {
 		if (!editingMessage) return null
+		const content = String(editingMessage?.content ?? '').trim()
 
 		return (
 			<Flex className={classes.chatReply}>
 				<ReplyIcon />
 				<Flex className={classes.replyInfo} vertical>
 					<div className={classes.replyName}>Editing message</div>
+					{content ? (
+						<div className={classes.text}>
+							<div>{editingMessage.content}</div>
+						</div>
+					) : null}
 					{_renderEditMediaThumbnails()}
 				</Flex>
 				<Flex className={classes.replyCancel} onClick={handleCancelEditMode}>
